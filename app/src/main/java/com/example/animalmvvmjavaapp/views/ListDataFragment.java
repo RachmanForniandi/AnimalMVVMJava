f3d7dea9817b0f0fc3f40a5d9e3351469e6c7ed0
RachmanForniandi <rachmanforniandi@gmail.com>
2020-06-21 16:25:09 +0700

    issue class direction navigation solved.

--- app/src/main/java/com/example/animalmvvmjavaapp/views/ListDataFragment.java
+++ app/src/main/java/com/example/animalmvvmjavaapp/views/ListDataFragment.java
@@ -2,15 +2,26 @@ package com.example.animalmvvmjavaapp.views;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavDirections;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.animalmvvmjavaapp.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class ListDataFragment extends Fragment {
+
+    @BindView(R.id.fab_1)
+    FloatingActionButton listFab;
 
     public ListDataFragment() {
         // Required empty public constructor
@@ -20,6 +31,17 @@ public class ListDataFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list_data, container, false);
+        View view =inflater.inflate(R.layout.fragment_list_data, container, false);
+        ButterKnife.bind(this,view);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        listFab.setOnClickListener(view1 -> {
+            NavDirections navDirections = ListDataFragmentDirections.actionGoToDetails();
+            Navigation.findNavController(view1).navigate(navDirections);
+        });
     }
 }
